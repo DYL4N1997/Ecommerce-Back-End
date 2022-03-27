cac729d65f3e018519fa58509dd099588ab2611b
Dylan <user@users-MacBook-Pro-2.local>
2022-03-27 23:06:07 +0100

    Updating async routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,6 +63,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await Category.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
 });
 
 module.exports = router;
