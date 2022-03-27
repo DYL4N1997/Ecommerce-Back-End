5cc47322132f7572d10a312c961594e7a8811c28
Dylan <user@users-MBP-2.broadband>
2022-03-27 17:23:20 +0100

    Updating model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,30 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INT,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    product_name: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    
+    }
+
+    }
   },
   {
     sequelize,
