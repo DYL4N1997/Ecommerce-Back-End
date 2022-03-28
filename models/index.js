1aa397a741e7a91321173c66e2d2989242b73fec
Dylan <user@users-MBP-2.broadband>
2022-03-28 22:07:49 +0100

    Updating index model and relationships

--- models/index.js
+++ models/index.js
@@ -10,18 +10,23 @@ Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
 
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+
+// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id",
 });
 
-// Categories have many Products
-Category.has
-
-
-// Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
