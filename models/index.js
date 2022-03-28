d006c733960581c30d8adaaddd9222944b5b794a
Dylan <user@users-MBP-2.broadband>
2022-03-28 22:05:11 +0100

    Updating index model and relationships

--- models/index.js
+++ models/index.js
@@ -5,8 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  onDelete: "CASCADE",
+  foreignKey: "category_id",
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Categories have many Products
+Category.has
+
 
 // Products belongToMany Tags (through ProductTag)
 
